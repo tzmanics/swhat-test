ad14a26bf64c71b7b2a1ae4c451cd1c897062822
tzmanics <tzmanics@gmail.com>
2018-03-16 00:04:04 -0400

    delete the old, add term comp, add KUI

--- src/main.js
+++ src/main.js
@@ -2,12 +2,29 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import {
+  ButtonGroupButton,
+  ButtonsInstaller
+} from '@progress/kendo-buttons-vue-wrapper'
+import {
+  Chart,
+  ChartSeriesItem,
+  ChartInstaller
+} from '@progress/kendo-charts-vue-wrapper'
+import '@progress/kendo-theme-default/dist/all.css'
+import '@progress/kendo-ui'
 
 Vue.config.productionTip = false
+Vue.use(ButtonsInstaller, ChartInstaller)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   template: '<App/>',
-  components: { App }
+  components: {
+    App,
+    ButtonGroupButton,
+    Chart,
+    ChartSeriesItem
+  }
 })
